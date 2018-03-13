0fc86c498dcb936cb8943797ed69b4285fd5881d
Ganesh Raut <ganeshr@iprogrammer.com>
2018-03-13 19:31:40 +0530

    First commit

---
+++ app/src/main/java/com/mentor4mentee/kotlinapplibrary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mentor4mentee.kotlinapplibrary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
